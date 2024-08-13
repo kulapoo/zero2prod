cc658482af0e7cd53287da0440200bc4ad27ab89
kulapoo <kulaskulapoo@gmail.com>
2024-08-13 23:40:09 +0800

    add subscription tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+use actix_web::{HttpRequest, Responder};
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:0")
+        .expect("Failed to bind random port");
+    run(listener)?.await
+}
+
+#[allow(dead_code)]
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
 }
