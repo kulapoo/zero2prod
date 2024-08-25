e31f1c9c998df9ab6dd0a41996ca67b296e71b5c
kulapoo <kulaskulapoo@gmail.com>
2024-08-25 22:27:15 +0800

    add user & password - test success

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -8,7 +8,7 @@ use actix_web::{
 };
 use anyhow::Context;
 use base64::Engine;
-use secrecy::Secret;
+use secrecy::{ExposeSecret, Secret};
 use sqlx::PgPool;
 
 use crate::{domain::SubscriberEmail, email_client::EmailClient, routes::error_chain_fmt};
@@ -101,13 +101,46 @@ fn basic_authentication(headers: &HeaderMap) -> Result<Credentials, anyhow::Erro
     })
 }
 
+async fn validate_credentials(
+    credentials: Credentials,
+    pool: &PgPool,
+) -> Result<uuid::Uuid, PublishError> {
+    let user_id: Option<_> = sqlx::query!(
+        r#"
+SELECT user_id
+FROM users
+WHERE username = $1 AND password = $2
+"#,
+        credentials.username,
+        credentials.password.expose_secret()
+    )
+    .fetch_optional(pool)
+    .await
+    .context("Failed to perform a query to validate auth credentials.")
+    .map_err(PublishError::UnexpectedError)?;
+    user_id
+        .map(|row| row.user_id)
+        .ok_or_else(|| anyhow::anyhow!("Invalid username or password."))
+        .map_err(PublishError::AuthError)
+}
+
+#[tracing::instrument(
+    name = "Publish a newsletter issue",
+    skip(body, pool, email_client, request),
+    fields(username=tracing::field::Empty, user_id=tracing::field::Empty)
+)]
 pub async fn publish_newsletter(
     body: web::Json<BodyData>,
     pool: web::Data<PgPool>,
     email_client: web::Data<EmailClient>,
     request: HttpRequest,
 ) -> Result<HttpResponse, PublishError> {
-    let _credentials = basic_authentication(request.headers()).map_err(PublishError::AuthError)?;
+    let credentials = basic_authentication(request.headers()).map_err(PublishError::AuthError)?;
+
+    tracing::Span::current().record("username", &tracing::field::display(&credentials.username));
+    let user_id = validate_credentials(credentials, &pool).await?;
+    tracing::Span::current().record("user_id", &tracing::field::display(&user_id));
+
     let subscribers = get_confirmed_subscribers(&pool).await?;
 
     for subscriber in subscribers {
@@ -130,13 +163,13 @@ pub async fn publish_newsletter(
             }
             Err(error) => {
                 tracing::warn!(
-                // We record the error chain as a structured field
-                // on the log record.
-                error.cause_chain = ?error,
-                // Using `\` to split a long string literal over
-                // two lines, without creating a `\n` character.
-                "Skipping a confirmed subscriber. \
-                Their stored contact details are invalid",
+                    // We record the error chain as a structured field
+                    // on the log record.
+                    error.cause_chain = ?error,
+                    // Using `\` to split a long string literal over
+                    // two lines, without creating a `\n` character.
+                    "Skipping a confirmed subscriber. \
+                    Their stored contact details are invalid",
                 );
             }
         }
