285fccae0ee4df133d506fd7e33adb5556bdb8ed
kulapoo <kulaskulapoo@gmail.com>
2024-08-30 11:36:24 +0800

    add logoout

--- src/startup.rs
+++ src/startup.rs
@@ -104,6 +104,7 @@ pub async fn run(
                 web::get().to(routes::change_password_form),
             )
             .route("/admin/password", web::post().to(routes::change_password))
+            .route("/admin/logout", web::post().to(routes::log_out))
             .route("/", web::get().to(routes::home))
             .route("/login", web::get().to(routes::login_form))
             .route("/login", web::post().to(routes::login))
