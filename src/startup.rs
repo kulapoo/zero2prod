f827db98fc9907a4141b2ffe42d9a6156b5e89fd
kulapoo <kulaskulapoo@gmail.com>
2024-09-03 01:37:17 +0800

    ch10 completed - test pass

--- src/startup.rs
+++ src/startup.rs
@@ -99,14 +99,18 @@ pub async fn run(
             .route("/health_check", web::get().to(routes::health_check))
             .route("/subscriptions", web::post().to(routes::subscribe))
             .route("/subscriptions/confirm", web::get().to(routes::confirm))
-            .route("/newsletters", web::post().to(routes::publish_newsletter))
             .service(
                 web::scope("/admin")
                     .wrap(from_fn(reject_anonymous_users))
                     .route("/dashboard", web::get().to(routes::admin_dashboard))
                     .route("/password", web::get().to(routes::change_password_form))
                     .route("/password", web::post().to(routes::change_password))
-                    .route("/logout", web::post().to(routes::log_out)),
+                    .route("/logout", web::post().to(routes::log_out))
+                    .route("/newsletters", web::post().to(routes::publish_newsletter))
+                    .route(
+                        "/newsletters",
+                        web::get().to(routes::publish_newsletter_form),
+                    ),
             )
             .route("/", web::get().to(routes::home))
             .route("/login", web::get().to(routes::login_form))
